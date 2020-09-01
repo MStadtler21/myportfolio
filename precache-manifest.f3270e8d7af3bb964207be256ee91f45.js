15c4e22ae10bcc9cb2a0696d48f840f73977755b
MatStat <powdersteamboat@gmail.com>
2020-08-31 19:08:06 -0600

    Updates

---
+++ precache-manifest.f3270e8d7af3bb964207be256ee91f45.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a9e763653d148711241d661c4bff23",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "6f64735588d5c5ae5742",
+    "url": "/myportfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "315349f5f699cbb20e45",
+    "url": "/myportfolio/static/css/main.fe798c0b.chunk.css"
+  },
+  {
+    "revision": "6f64735588d5c5ae5742",
+    "url": "/myportfolio/static/js/2.4f475f54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.4f475f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315349f5f699cbb20e45",
+    "url": "/myportfolio/static/js/main.8642e9cc.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "ec1fcb3fa5270a893ac2baeaf5e1ee73",
+    "url": "/myportfolio/static/media/brewgle.ec1fcb3f.PNG"
+  },
+  {
+    "revision": "1e66db4e2b2c18391230784ad07f05a8",
+    "url": "/myportfolio/static/media/chatify.1e66db4e.PNG"
+  },
+  {
+    "revision": "86faed2d165a5318c2fb66955eb0040b",
+    "url": "/myportfolio/static/media/employee.86faed2d.PNG"
+  },
+  {
+    "revision": "f49e57608c65385a13bbb5dce69d02f0",
+    "url": "/myportfolio/static/media/fitnesstracker.f49e5760.PNG"
+  },
+  {
+    "revision": "52c1ed96faebe4a9b6469ce627a1f9cf",
+    "url": "/myportfolio/static/media/notetaker.52c1ed96.PNG"
+  },
+  {
+    "revision": "e42c08da824e7e73a3bde4f15bd1576a",
+    "url": "/myportfolio/static/media/weather.e42c08da.PNG"
+  }
+]);
